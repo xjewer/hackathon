5127bd04981c06e7d1afcc7616708429b59d214a
Renat Akhmedyanov <renat@topface.com>
2015-05-13 19:40:33 +0300

    fixes

--- hackathon-app/hackathon-app-entrypoint.sh
+++ hackathon-app/hackathon-app-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 9000/g' /etc/php5/fpm/pool.d/www.conf
-/etc/init.d/php5-fpm start
-/etc/init.d/nginx start
+#/etc/init.d/php5-fpm start
+#/etc/init.d/nginx start
 exec "$@"
